dcf16d8285235c93f3337374a9dba5ce74420790
php 07 <php.07@metralha.cec.ime.usp.br>
2017-02-13 22:37:38 -0200

    Criando a página ola

---
+++ resources/views/ola.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Olá View</title>
+    <link rel="stylesheet" href="/css/master.css">
+  </head>
+  <body>
+    <div class="container">
+      @for($i = 0; $i < 10; $i++)
+        <h2>Boa noite Ezequias</h2>
+      @endfor
+    </div>
+    <script type="text/javascript" src=/js/app.js>
+
+    </script>
+  </body>
+</html>
